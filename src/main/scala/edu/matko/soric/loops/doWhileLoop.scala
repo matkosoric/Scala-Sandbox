7dcd824b81c62a1628e320f84ac74a58e5734a1e
matko-CT <soric.matko@gmail.com>
2019-01-23 15:15:12 +0100

    do-while loop example

---
+++ src/main/scala/edu/matko/soric/loops/doWhileLoop.scala
@@ -0,0 +1,31 @@
+package edu.matko.soric.loops
+
+object doWhileLoop {
+
+  def main(args: Array[String]) {
+
+    var year = 1990
+
+    // this will print every year between 1990 and 2000
+    do {
+      println("Current year: " + year)
+      year = year + 1
+    } while ( {
+      year <= 2000
+    })
+
+    print("\n")
+    // this will still print year once, since block is executed at least once,
+    // despite the fact that it is intended to print only years smaller than 1980.
+    // in this case, do loop would be a proper choice
+
+    do {
+      println("Current year: " + year)
+      year = year + 1
+    } while ( {
+      year <= 1980
+    })
+
+  }
+
+}
